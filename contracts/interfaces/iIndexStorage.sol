514898558b6945d904bb6d1f5737c32d26d65fab
stanta <st.takt@gmail.com>
2020-10-02 22:08:48 +0300

    some fixes

--- contracts/interfaces/iIndexStorage.sol
+++ contracts/interfaces/iIndexStorage.sol
@@ -6,7 +6,7 @@ interface iIndexStorage {
     //mapping (bytes32 => address) public indexes ;
     function indexes (bytes32 _code ) external view returns (address);
 
-    function setIndex (string memory _name, string memory _symbol, address _index) external ;
+    function setIndex (string calldata _name, string calldata _symbol, address _index) external ;
 
     function getLenIndexes () external view  returns (uint);
 
