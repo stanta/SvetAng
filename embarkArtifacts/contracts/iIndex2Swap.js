a267d975869c84a2be6cd910318c6095c70b15e5
stanta <st.takt@gmail.com>
2020-10-12 08:50:36 +0300

    deploy Ropsten  1210-0845

---
+++ embarkArtifacts/contracts/iIndex2Swap.js
@@ -0,0 +1,6 @@
+import EmbarkJS from '../embarkjs';
+
+const iIndex2SwapConfig = {"abiDefinition":[],"className":"iIndex2Swap","args":[],"gas":"auto","silent":false,"track":true,"deploy":false,"realRuntimeBytecode":"","realArgs":[],"code":"","runtimeBytecode":"","linkReferences":{},"swarmHash":"","gasEstimates":null,"functionHashes":{},"filename":"/home/st/Desktop/SvetAng2/SvetAng/.embark/contracts/interfaces/iIndex2Swap.sol","originalFilename":"contracts/interfaces/iIndex2Swap.sol","path":"/home/st/Desktop/SvetAng2/SvetAng/contracts/interfaces/iIndex2Swap.sol","type":"file","deploymentAccount":"0xcDD7C0fba0Fe0D7DDf49a0B6B627700F4e26b6F8"};
+const iIndex2Swap = new EmbarkJS.Blockchain.Contract(iIndex2SwapConfig);
+
+export default iIndex2Swap;
---
+++ embarkArtifacts/contracts/iLstorage.js
@@ -0,0 +1,6 @@
+import EmbarkJS from '../embarkjs';
+
+const iLstorageConfig = {"abiDefinition":[{"inputs":[{"internalType":"address","name":"_addrIndex","type":"address"},{"internalType":"address","name":"_addrA","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"add","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_addrIndex","type":"address"},{"internalType":"address","name":"_addrA","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"sub","outputs":[],"stateMutability":"nonpayable","type":"function"}],"className":"iLstorage","args":[],"gas":"auto","silent":false,"track":true,"deploy":false,"realRuntimeBytecode":"","realArgs":[],"code":"","runtimeBytecode":"","linkReferences":{},"swarmHash":"","gasEstimates":null,"functionHashes":{"add(address,address,uint256)":"551f8e2a","sub(address,address,uint256)":"55ceeb31"},"filename":"/home/st/Desktop/SvetAng2/SvetAng/.embark/contracts/interfaces/iLstorage.sol","originalFilename":"contracts/interfaces/iLstorage.sol","path":"/home/st/Desktop/SvetAng2/SvetAng/contracts/interfaces/iLstorage.sol","type":"file","deploymentAccount":"0xcDD7C0fba0Fe0D7DDf49a0B6B627700F4e26b6F8"};
+const iLstorage = new EmbarkJS.Blockchain.Contract(iLstorageConfig);
+
+export default iLstorage;
