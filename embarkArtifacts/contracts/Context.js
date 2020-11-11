2d2325a6c34cb7328c1a08ce899ce1d3c7745bfc
stanta <st.takt@gmail.com>
2020-11-11 22:26:42 +0300

    added artifacts from Ropsten

---
+++ embarkArtifacts/contracts/Context.js
@@ -0,0 +1,6 @@
+import EmbarkJS from '../embarkjs';
+
+const ContextConfig = {"abiDefinition":[],"className":"Context","args":[],"gas":"auto","silent":false,"track":true,"deploy":false,"realRuntimeBytecode":"","realArgs":[],"code":"","runtimeBytecode":"","linkReferences":{},"swarmHash":"","gasEstimates":null,"functionHashes":{},"filename":"/home/st/Desktop/SvetAng2/SvetAng/.embark/node_modules/@openzeppelin/contracts/GSN/Context.sol","originalFilename":"contracts//home/st/Desktop/SvetAng2/SvetAng/.embark/node_modules/@openzeppelin/contracts/GSN/Context.sol","path":"/home/st/Desktop/SvetAng2/SvetAng/contracts/.embark/node_modules/@openzeppelin/contracts/GSN/Context.sol","type":"file","deploymentAccount":"0xcDD7C0fba0Fe0D7DDf49a0B6B627700F4e26b6F8"};
+const Context = new EmbarkJS.Blockchain.Contract(ContextConfig);
+
+export default Context;
